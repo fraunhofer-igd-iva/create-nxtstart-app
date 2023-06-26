2e1b1f059e64439a181680fe911a10dbad03fd44
fwagner <felix.wagner@igd.fraunhofer.de>
2023-06-26 14:58:45 +0200

    remove prisma from d3 example

--- src/templates/d3/src/pages/d3.tsx
+++ src/templates/d3/src/pages/d3.tsx
@@ -4,7 +4,6 @@ import Head from 'next/head';
 import {Data} from '../util/types';
 import {serverSideTranslations} from 'next-i18next/serverSideTranslations';
 import BarChart from '../components/d3/BarChart';
-import {prisma} from '../util/prismaClient';
 import MuiNextLink from '../components/MuiNextLink';
 import ScatterPlot, {DataPoint} from '../components/d3/ScatterPlot';
 
@@ -14,10 +13,59 @@ interface D3PageProps {
 }
 
 export async function getStaticProps({ locale }: any) {
-    const cities = await prisma.city.findMany({where: {Population: {gte: 1000000}}, orderBy: {Population: 'desc'}, take: 50})
-    const dataCities: Data[] = cities.map(c => {
-        return {label: c.Name, value: c.Population, totalDataPoints: cities.length}
-    })
+    const dataCities: Data[] = [
+        { label: 'Mumbai (Bombay)', value: 10500000, totalDataPoints: 50 },
+        { label: 'Seoul', value: 9981619, totalDataPoints: 50 },
+        { label: 'São Paulo', value: 9968485, totalDataPoints: 50 },
+        { label: 'Shanghai', value: 9696300, totalDataPoints: 50 },
+        { label: 'Jakarta', value: 9604900, totalDataPoints: 50 },
+        { label: 'Karachi', value: 9269265, totalDataPoints: 50 },
+        { label: 'Istanbul', value: 8787958, totalDataPoints: 50 },
+        { label: 'Ciudad de México', value: 8591309, totalDataPoints: 50 },
+        { label: 'Moscow', value: 8389200, totalDataPoints: 50 },
+        { label: 'New York', value: 8008278, totalDataPoints: 50 },
+        { label: 'Tokyo', value: 7980230, totalDataPoints: 50 },
+        { label: 'Peking', value: 7472000, totalDataPoints: 50 },
+        { label: 'London', value: 7285000, totalDataPoints: 50 },
+        { label: 'Delhi', value: 7206704, totalDataPoints: 50 },
+        { label: 'Cairo', value: 6789479, totalDataPoints: 50 },
+        { label: 'Teheran', value: 6758845, totalDataPoints: 50 },
+        { label: 'Lima', value: 6464693, totalDataPoints: 50 },
+        { label: 'Chongqing', value: 6351600, totalDataPoints: 50 },
+        { label: 'Bangkok', value: 6320174, totalDataPoints: 50 },
+        { label: 'Santafé de Bogotá', value: 6260862, totalDataPoints: 50 },
+        { label: 'Rio de Janeiro', value: 5598953, totalDataPoints: 50 },
+        { label: 'Tianjin', value: 5286800, totalDataPoints: 50 },
+        { label: 'Kinshasa', value: 5064000, totalDataPoints: 50 },
+        { label: 'Lahore', value: 5063499, totalDataPoints: 50 },
+        { label: 'Santiago de Chile', value: 4703954, totalDataPoints: 50 },
+        { label: 'St Petersburg', value: 4694000, totalDataPoints: 50 },
+        { label: 'Calcutta [Kolkata]', value: 4399819, totalDataPoints: 50 },
+        { label: 'Wuhan', value: 4344600, totalDataPoints: 50 },
+        { label: 'Baghdad', value: 4336000, totalDataPoints: 50 },
+        { label: 'Harbin', value: 4289800, totalDataPoints: 50 },
+        { label: 'Shenyang', value: 4265200, totalDataPoints: 50 },
+        { label: 'Kanton [Guangzhou]', value: 4256300, totalDataPoints: 50 },
+        { label: 'Singapore', value: 4017733, totalDataPoints: 50 },
+        { label: 'Ho Chi Minh City', value: 3980000, totalDataPoints: 50 },
+        { label: 'Chennai (Madras)', value: 3841396, totalDataPoints: 50 },
+        { label: 'Pusan', value: 3804522, totalDataPoints: 50 },
+        { label: 'Los Angeles', value: 3694820, totalDataPoints: 50 },
+        { label: 'Dhaka', value: 3612850, totalDataPoints: 50 },
+        { label: 'Berlin', value: 3386667, totalDataPoints: 50 },
+        { label: 'Rangoon (Yangon)', value: 3361700, totalDataPoints: 50 },
+        { label: 'Chengdu', value: 3361500, totalDataPoints: 50 },
+        { label: 'Jokohama [Yokohama]', value: 3339594, totalDataPoints: 50 },
+        { label: 'Alexandria', value: 3328196, totalDataPoints: 50 },
+        { label: 'Riyadh', value: 3324000, totalDataPoints: 50 },
+        { label: 'Sydney', value: 3276207, totalDataPoints: 50 },
+        { label: 'Ankara', value: 3038159, totalDataPoints: 50 },
+        { label: 'Buenos Aires', value: 2982146, totalDataPoints: 50 },
+        { label: 'Hyderabad', value: 2964638, totalDataPoints: 50 },
+        { label: 'Casablanca', value: 2940623, totalDataPoints: 50 },
+        { label: 'Chicago', value: 2896016, totalDataPoints: 50 }
+      ]
+      
 
     const x: number[] = Array.from({length: 40}, () => Math.floor(Math.random() * 40))
     const y: number[] = Array.from({length: 40}, () => Math.floor(Math.random() * 40))
