c78953859571fe61dcda3b05bc6bd33fd04abca3
Felix Wagner <felixwagner99@googlemail.com>
2023-07-20 12:36:58 +0200

    add generalized version for next config

---
+++ src/templates/general/next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+<%i18n%>const { i18n } = require('./next-i18next.config')</%i18n%>
+<%pwa%>// disable service worker in development to prevent warning spam https://github.com/GoogleChrome/workbox/issues/1790.
+// enable again to test service worker locally
+const withPWA = require('next-pwa')({dest: 'public', disable: process.env.NODE_ENV === 'development'})</%pwa%>
+
+const nextConfig = {
+  reactStrictMode: true,
+  <%i18n%>i18n: i18n,</%i18n%>
+}
+
+module.exports = <%pwa%>withPWA(</%pwa%>nextConfig<%pwa%>)</%pwa%>
