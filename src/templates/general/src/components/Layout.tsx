374acd0c17b84103b0a3fa6668aaddebcadf19ce
Felix Wagner <felixwagner99@googlemail.com>
2023-08-07 11:22:31 +0200

    remove "

--- src/templates/general/src/components/Layout.tsx
+++ src/templates/general/src/components/Layout.tsx
@@ -36,9 +36,9 @@ export default function Layout(props: LayoutProps) {
         <%animations%><motion.main
           key={router.asPath}
           variants={variants} // Pass the variant object into Framer Motion
-          initial="hidden" // Set the initial state to variants.hidden
-          animate="enter" // Animated state to variants.enter
-          exit="exit" // Exit state (used later) to variants.exit
+          initial={'hidden'} // Set the initial state to variants.hidden
+          animate={'enter'} // Animated state to variants.enter
+          exit={'exit'} // Exit state (used later) to variants.exit
           transition={{ type: 'linear' }} // Set the transition to linear
         ></%animations%>
           {props.children}
