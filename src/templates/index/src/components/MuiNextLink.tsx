bbca7bec46dc11629a232e5a77991d6263a1744c
Felix Wagner <felixwagner99@googlemail.com>
2023-08-31 11:03:46 +0200

    add props to make custom link more dynamic

--- src/templates/index/src/components/MuiNextLink.tsx
+++ src/templates/index/src/components/MuiNextLink.tsx
@@ -6,6 +6,8 @@ interface MuiNextLinkProps {
   href: string
   label: string
   sx?: SxProps<Theme>
+  noDecoration?: boolean
+  openInNewTab?: boolean
 }
 
 // link component combining next link functionality with mui design
@@ -13,18 +15,19 @@ export default function MuiNextLink(props: MuiNextLinkProps) {
   const theme = useTheme()
 
   return (
-    <Link 
-      href={props.href} 
-      component={NextLink} 
+    <Link
+      href={props.href}
+      component={NextLink}
       sx={{
         color: theme.palette.primary.main,
-        textDecorationColor: theme.palette.primary.light,
+        textDecorationColor: props.noDecoration ? 'transparent' : theme.palette.primary.main,
         '&:hover': {
           color: theme.palette.primary.dark,
-          textDecorationColor: theme.palette.primary.dark,
-        }, 
+          textDecorationColor: props.noDecoration ? 'transparent' : theme.palette.primary.dark,
+        },
         ...props.sx,
       }}
+      target={props.openInNewTab ? '_blank' : undefined}
     >
       {props.label}
     </Link>
