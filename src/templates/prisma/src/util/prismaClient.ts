6fd6099c0b64109c48422604f9adb2bd0d7808d6
Felix Wagner <felixwagner99@googlemail.com>
2023-10-30 15:46:48 +0100

    change logging levels for prisma

--- src/templates/prisma/src/util/prismaClient.ts
+++ src/templates/prisma/src/util/prismaClient.ts
@@ -8,7 +8,7 @@ const globalForPrisma = global as unknown as {
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ['query'],
+    log: ['query', 'info', 'warn', 'error'],
   })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
