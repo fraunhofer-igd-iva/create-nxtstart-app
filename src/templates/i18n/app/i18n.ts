de868803bd323c3afda9d8f8638cb3dc0eaf2a92
Felix Wagner <felixwagner99@googlemail.com>
2023-12-11 15:01:11 +0100

    fix module error with next 14.0.4

--- src/templates/i18n/app/i18n.ts
+++ src/templates/i18n/app/i18n.ts
@@ -8,7 +8,7 @@ export default async function initTranslations(locale: string, namespaces: strin
 
   await i18nInstance
     .use(initReactI18next)
-    .use(resourcesToBackend((language: string, namespace: string) => import(`locales/${language}/${namespace}.json`)))
+    .use(resourcesToBackend((language: string, namespace: string) => import(`@/locales/${language}/${namespace}.json`)))
     .init({
       lng: locale,
       fallbackLng: i18nConfig.defaultLocale,
