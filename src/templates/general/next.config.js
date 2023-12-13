bc31433b6b8ac6bb3455dd5c1aefd7af9b301e9d
Felix Wagner <felixwagner99@googlemail.com>
2023-12-13 11:06:50 +0100

    fix typo in comment

--- src/templates/general/next.config.js
+++ src/templates/general/next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
   poweredByHeader: false,
   reactStrictMode: true,
   // comment in if local build fails due to "glob error [Error: EPERM: operation not permitted, scandir <path>"
-  // deprecated and will be removed in Next.js 15
+  // deprecated and will be removed in NextJS 15
   //outputFileTracing: false,
 }
 
