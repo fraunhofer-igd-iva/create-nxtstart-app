52fc1f53bc44a5aa087ac08b54fbc85998aa76dc
Felix Wagner <felixwagner99@googlemail.com>
2024-05-27 15:30:25 +0200

    fix wrong i18n cookie initialization

--- src/templates/general/components/LanguageSelector.tsx
+++ src/templates/general/components/LanguageSelector.tsx
@@ -20,7 +20,7 @@ export default function LanguageSelector() {
     const days = 30
     const date = new Date()
     date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000)
-    const expires = '; expires=' + date.toUTCString()
+    const expires = date.toUTCString()
     document.cookie = `NEXT_LOCALE=${newLocale};expires=${expires};path=/`
 
     // redirect to the new locale path
