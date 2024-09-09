6c132d70f766eb5aa1091e947ede2bf7928c35f8
Felix Wagner <felixwagner99@googlemail.com>
2024-09-09 13:25:23 +0200

    fix pwa install feature

--- src/templates/general/app/[locale]/layout.tsx
+++ src/templates/general/app/[locale]/layout.tsx
@@ -36,6 +36,18 @@ export default async function RootLayout({
 
   return (
     <html lang={locale}>
+      <§pwa§><head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png" />
+        <link rel="manifest" href="/img/site.webmanifest" />
+        <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#00c853" />
+        <link rel="shortcut icon" href="/img/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#00c853" />
+        <meta name="msapplication-config" content="/img/browserconfig.xml" />
+        <meta name="theme-color" content="#000000" />
+      </head></§pwa§>
       <body className={inter.className}>
         <AppRouterCacheProvider>
           <TranslationProvider namespaces={options.ns} locale={locale}>
