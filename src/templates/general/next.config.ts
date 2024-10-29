9c2f6bc64c9028c1f0c0dc41fabe098d1b83b1be
Felix Wagner <felixwagner99@googlemail.com>
2024-10-29 13:35:58 +0100

    remove deprecated nextjs option

--- src/templates/general/next.config.ts
+++ src/templates/general/next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   poweredByHeader: false,
   reactStrictMode: true,
-  // comment in if local build fails due to "glob error [Error: EPERM: operation not permitted, scandir <path>"
-  // deprecated and will be removed in NextJS 15
-  //outputFileTracing: false,
 }
 
 export default nextConfig
