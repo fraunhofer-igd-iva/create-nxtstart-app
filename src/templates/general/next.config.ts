ef0480dde154fd03ef7e39f403ffbd7f6524bef2
Felix Wagner <felixwagner99@googlemail.com>
2024-11-07 11:22:13 +0100

    simplify dockerfile, add dockerignore, use standalone output in production, reduce image size significantly

--- src/templates/general/next.config.ts
+++ src/templates/general/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: process.env.BUILD_STANDALONE === '1' ? 'standalone' : undefined,
   poweredByHeader: false,
   reactStrictMode: true,
 }
